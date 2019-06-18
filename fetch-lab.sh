99d0fa4dd9689dab239acbbc8b7c5a67652b634d
WiredQuill <wiredquill@gmail.com>
2019-06-18 12:47:09 -0700

    Created fetch-lab.sh

---
+++ fetch-lab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/tux
+mkdir lab-config
+git clone https://github.com/wiredquill/lab-config lab-config
+# cd /home/tux/lab-config
+# rm -rf /home/tux/course_files/CAAS101/*
+#cd /home/tux/course_files/CAAS101/
+#cp -r /home/tux/cffetch/course_files/* .
