fb8d7885223f9a68dc41ac7ae9154016a6ad84d3
WiredQuill <wiredquill@gmail.com>
2019-06-18 13:06:52 -0700

    Updated fetch-lab.sh to download from Wiredquill/lab-config and copy file to /srv/..

--- fetch-lab.sh
+++ fetch-lab.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd /home/tux
-mkdir lab-config
-git clone https://github.com/wiredquill/lab-config lab-config
-# cd /home/tux/lab-config
-# rm -rf /home/tux/course_files/CAAS101/*
-#cd /home/tux/course_files/CAAS101/
-#cp -r /home/tux/cffetch/course_files/* .
+cd /home/tux/lab-config
+git pull
+cp -r /home/tux/lab-config/srv* /srv/
