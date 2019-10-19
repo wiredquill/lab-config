ea113e25b0200896811b82c1690f790ff7a8e196
WiredQuill <wiredquill@gmail.com>
2019-10-19 09:20:16 -0700

    Created install-lab script

---
+++ install-lab.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+echo Updating Raspbian
+echo
+sudo apt update
+sudo apt upgrade --yes
+sudo apt install vim --yes
+echo 
+echo Done updating Rapsbian
+
+echo
+echo installing Fanshim
+
+cd
+git clone https://github.com/pimoroni/fanshim-python
+cd fanshim-python
+sudo ./install.sh
+cd examples
+sudo ./install-service.sh --on-threshold 65 --off-threshold 55 --delay 2
+cd
+echo 
+echo Done installing Fanshim
+
+echo Installing Latest Node-Red
+echo 
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+
+echo
+echo Done installing Node Red
+
+
+echo updating Node Red to support Amazon Echo and Project
+echo 
+sudo cp /home/pi/lab-config/lib/systemd/system/nodered.service /lib/systemd/system
+sudo systemctl enable --now nodered.service
+sudo systemctl stop nodered.service
+cp /home/pi/lab-config/node-red /home/pi/.node-red
+sudo systemctl start nodered.service
+
+echo done updating Node Red
+echo
+echo copying boot config
+sudo cp /home/pi/boot /boot
+
+echo
+echo reboot to activate
+
+cd ~/lab-config
+git pull
+cp -r /home/tux/lab-config/srv/* /srv/
